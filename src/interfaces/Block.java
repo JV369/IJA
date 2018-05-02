87bb5aaf616db9dd3ccfbb99888a66d60d65ec63
jv369 <xvavra20@vutbr.cz>
2018-05-02 18:42:23 +0200

    interface javadoc

--- src/interfaces/Block.java
+++ src/interfaces/Block.java
@@ -2,14 +2,47 @@ package interfaces;
 
 import components.Port;
 
+/**
+ * Rozhraní pro blok
+ * @author Jan Vávra (xvavra20)
+ * @author Aleš Postulka (xpostu03)
+ */
 public interface Block {
+    /**
+     * Provede akci nad blokem
+     */
     void execute();
-    int getId();
-    double[] getCoordinates();
-    Port getInPort(int index);
-    Port getOutPort(int index);
-    //maybe?
-    void setCoordinates(double newX, double newY);
-    //end maybe?
 
+    /**
+     * Vrátí id bloku
+     * @return id
+     */
+    int getId();
+
+    /**
+     * Získá hodnoty x,y bloku ve schématu
+     * @return 2prvkové pole [x,y]
+     */
+    double[] getCoordinates();
+
+    /**
+     * Vrátí port z pole vstupních portů na indexu "index"
+     * @param index index portu v poli
+     * @return vstupní port
+     */
+    Port getInPort(int index);
+    /**
+     * Vrátí port z pole výstupních portů na indexu "index"
+     * @param index index portu v poli
+     * @return výstupní port
+     */
+    Port getOutPort(int index);
+
+    /**
+     * Nastaví x,y souřadnici portu
+     * Hodnoty se uchovávají pro případné uložení schématu
+     * @param newX nová x souřadnice
+     * @param newY nová y souřadnice
+     */
+    void setCoordinates(double newX, double newY);
 }
