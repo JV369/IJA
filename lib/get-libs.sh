0bf61e150f8a7c276b8781ae48e2f87aed94c848
jv369 <xvavra20@vutbr.cz>
2018-05-06 14:00:17 +0200

    finalv2

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+rm -f junit-4.12.jar;
+rm -f hamcrest-core-1.3.jar;
 wget https://github.com/junit-team/junit4/releases/download/r4.12/junit-4.12.jar;
 wget repo1.maven.org/maven2/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar;
